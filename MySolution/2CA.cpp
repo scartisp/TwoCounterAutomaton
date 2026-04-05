9362313d4091a0af11754945a9bd4513b294231f
scartisp <simion.cartis1@gmail.com>
2026-04-05 19:31:49 -0400

    read .txt file into vector of Transition structs

--- MySolution/2CA.cpp
+++ MySolution/2CA.cpp
@@ -3,19 +3,85 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include<vector>
+#include<sstream>
+#include "functionHeaders.h"
 
 using namespace std;
-using std::cout;
-using std::endl;
-using std::string;
+//global variables
+vector<Transition> transitions;
 
 int main(int argc, char* argv[]) {
     string fileName;
-    if (argc == 2) {
+    if (argc == 2)
         fileName = argv[1];
-    } else {
+    else
         return 1;
-    }
-    cout << fileName;
+    create2CA(fileName);
     return 0;
+}
+
+void create2CA(string fileName) {
+    Transition t;
+    ifstream file(fileName);
+    if (!file) {
+        cerr << "no file found";
+        return;
+    }
+    string line;
+
+    while (getline(file, line)) {
+        // remove any comments from a line
+        size_t comment = line.find("//");
+        if (comment != string::npos)
+            line = line.substr(0, comment);
+        if (line.find_first_not_of(" \t") == string::npos)
+            continue;  // skips following code if line is just a comment
+
+        size_t arrowPos = line.find("->");
+        if (arrowPos == string::npos) {
+            cerr << "error during parsing";
+            return;
+        }
+        //get the two halfs of the line and extract the needed info
+        string left = trim(line.substr(0, arrowPos));
+        string right = trim(line.substr(arrowPos+2));
+        if (left.front() == '(') {left.erase(0, 1);}
+        if (left.back() == ')') {left.pop_back();} //get rid of the first () for each part
+        if (right.front() == '(') {right.erase(0, 1);}
+        if (right.back() == ')') {right.pop_back();}
+        vector<string> leftParts = splitByCama(left);
+        vector<string> rightParts = splitByCama(right);
+        //add info to the Transistion struct
+        t.currState = leftParts[0];
+        t.input = leftParts[1];
+        t.counterOneStat = leftParts[2];
+        t.counterTwoStat = leftParts[3];
+        t.nextState = rightParts[0];
+        t.changeCounterOne = rightParts[1];
+        t.changeCounterTwo = rightParts[2];
+        transitions.push_back(t);
+    }
+    file.close();
+}
+
+vector<string> splitByCama(string line) {
+    vector<string> parts;
+    string part;
+    stringstream lineStream(line);
+    while(getline(lineStream, part, ',')) {
+        parts.push_back(trim(part));
+    }
+    return parts;
+}
+
+string trim(string line) {
+    size_t start = line.find_first_not_of(" \t");
+    size_t end = line.find_last_not_of(" \t");
+    if (start == string::npos) {
+        return (" ");
+    }
+    return line.substr(start, end-start+1); // need to add 1 because valid string could only be 1 char
+                                            // also because 0 indexed, so for trim the left and right
+                                            // line halves, 0 - end + 1 gets you everything you need
 }
--- MySolution/functionHeaders.h
+++ MySolution/functionHeaders.h
@@ -1,4 +1,44 @@
 #ifndef _FUNCTIONHEADERS_H_
 #define _FUNCTIONHEADERS_H_
+#include<string>
+using std::string;
+#include<vector>
+using std::vector;
 
+/**
+ * struct for the transitions
+ */
+struct Transition {
+    //info from left side of a line
+    string currState;
+    string input;
+    string counterOneStat;
+    string counterTwoStat;
+    // infro from right side of a line
+    string nextState;
+    string changeCounterOne;
+    string changeCounterTwo;
+
+};
+/**
+ * function for creating the vector of Transition structs out of the .txt file given
+ * as an argument to this program
+ * @param {string} the name of the .txt file to read from
+ */
+void create2CA(string fileName);
+/**
+ * helper function to split each half of a line into their relevant pieces 
+ * (the halves are delineated) by a cama
+ * @param {string} the half of a string that we will extract needed info from
+ * @return a vector of strings containing the relevant parts pieces
+ */
+vector<string> splitByCama(string line);
+/**
+ * helper function to trim white spaces from parts of a line
+ * @param {string} the string which will have its white spaces removed
+ * @return {string} the version of the string that has its white spaces removed. 
+ * in the case that there is an epsilon transition (denoted by two camas with 0 or more white spaces)
+ * this function returns a whitespace to denote the epsilon transition
+ */
+string trim(string line);
 #endif
